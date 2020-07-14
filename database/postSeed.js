08dc6e9345216e8b3432e5e63c10fa79de531103
erichen886 <erichen886@gmail.com>
2020-07-14 16:01:59 -0700

    working script for user table

--- database/postSeed.js
+++ database/postSeed.js
@@ -1,11 +1,68 @@
 const fs = require('fs');
 const faker = require('faker');
-const argv = require('yargs').arv;
+const { argv } = require('yargs');
 
 const lines = argv.lines || 1000; //change this later;
-const filename = argv.output || 'postgres.csv';
-const writeStream = fs.createWriteStream(filename);
+const filename = argv.output || 'postgresUser.csv';
+const stream = fs.createWriteStream(filename);
 
-const createData = () => {
-  const user_id = faker.random
-}
+const createUsers = () => {
+  const email = faker.internet.email();
+  const first_name = faker.name.firstName();
+  const last_name = faker.name.lastName();
+
+  return `${email},${first_name},${last_name}\n`;
+
+};
+
+const createRooms = () => {
+  const title = faker.lorem.sentences();
+  const address = faker.address.streetAddress();
+
+  return `${title},${address}\n`;
+};
+
+const createList = () => {
+  const user_id = 1; // 1-1000
+  const name = faker.lorem.words();
+  const room_id = 1;// 1-1000
+};
+
+const createRoomPhotos = () => {
+  const image_url = faker.image.imageUrl;
+  const image_desc = faker.lorem.words();
+  const room_id = 1; // 1-1000
+};
+
+const startWriting = (writeStream, encoding, done) => {
+  let i = 0;
+  function writing() {
+    let canWrite = true;
+    do {
+      i += 1;
+      let user = createUsers();
+      // check if i === 0 so we would write and call `done`
+      if (i === lines) {
+        // we are done so fire callback
+        writeStream.write(user, encoding, done);
+      } else {
+        // else call write and continue looping
+        canWrite = writeStream.write(user, encoding);
+      }
+    } while (i < lines && canWrite);
+    if (i < lines && !canWrite) {
+      // our buffer for stream filled and need to wait for drain
+      // Write some more once it drains.
+      writeStream.once('drain', writing);
+    }
+  }
+  // initiate our writing function
+  writing();
+};
+
+// write our `header` line before we invoke the loop
+stream.write(`email,first_name,last_name\n`, 'utf-8');
+// invoke startWriting and pass callback
+startWriting(stream, 'utf-8', () => {
+  stream.end();
+});
--- database/schema.sql
+++ database/schema.sql
@@ -3,28 +3,29 @@ CREATE DATABASE sdcphotogallery;
 
 \c sdcphotogallery;
 
-CREATE TABLE users(
-id BIGSERIAL NOT NULL PRIMARY KEY,
-email varchar(30) NOT NULL,
+CREATE TABLE IF NOT EXISTS users(
+id SERIAL NOT NULL PRIMARY KEY,
+email varchar(50) NOT NULL,
 first_name varchar(20) NOT NULL,
-last_name varchar(20) NOT NULL,
+last_name varchar(20) NOT NULL
 );
 
-CREATE TABLE rooms (
-id BIGSERIAL NOT NULL PRIMARY KEY,
-title varchar (30) NOT NULL,
-address varchar (70) NOT NULL,
-);
+-- CREATE TABLE rooms (
+-- id SERIAL NOT NULL PRIMARY KEY,
+-- title varchar (30) NOT NULL,
+-- address varchar (70) NOT NULL
+-- );
 
-CREATE TABLE list (
-id BIGSERIAL NOT NULL PRIMARY KEY,
-user_Id BIGINT REFERENCES users(id),
-name varchar(30),
-room_id REFERENCES rooms(id)
-);
+-- CREATE TABLE list (
+-- id SERIAL NOT NULL PRIMARY KEY,
+-- user_Id INTEGER REFERENCES users(id),
+-- name varchar(30),
+-- room_id INTEGER REFERENCES rooms(id)
+-- );
 
-CREATE TABLE room_photos (
-id BIGSERIAL NOT NULL PRIMARY KEY,
-image_url varchar (50) NOT NULL,
-room_Id BIGINT REFERENCES rooms(id)
-)
+-- CREATE TABLE room_photos (
+-- id SERIAL NOT NULL PRIMARY KEY,
+-- image_url varchar (50) NOT NULL,
+-- image_desc varchar (70),
+-- room_Id INTEGER REFERENCES rooms(id)
+-- )
