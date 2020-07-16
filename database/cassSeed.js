5cf5ebe83c3f8d4b9dbe17c278bc399be8f1b8d7
erichen886 <erichen886@gmail.com>
2020-07-16 00:13:02 -0700

    cass seed and bash script

---
+++ database/cassSeed.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const faker = require('faker');
+const { argv } = require('yargs');
+
+const start = argv.start || 1;
+const finish = argv.finish || 2;
+
+const filename1 = argv.output1 || 'cassPhotosByRoom.csv';
+const filename2 = argv.output2 || 'cassListByUser.csv';
+
+const stream1 = fs.createWriteStream(filename1);
+const stream2 = fs.createWriteStream(filename2);
+
+const photosByRoom = (roomId) => {
+  const randomNumPhotos = Math.floor(Math.random() * 17);
+  const room_id = roomId;
+  let str = '';
+
+  for (let i = 1; i < randomNumPhotos; i += 1) {
+    const photo_order = i;
+    const title = faker.lorem.words();
+    const address = faker.address.streetAddress();
+    const image_url = faker.image.imageUrl();
+    const image_description = faker.lorem.words();
+
+    str += `${room_id},${photo_order},${title},${address},${image_url},${image_description}\n`;
+  }
+
+  return str;
+};
+
+const listByUser = (userId) => {
+  const randomNumList = Math.floor(Math.random() * 5);
+  const randomNumRoom = Math.floor(Math.random() * 8);
+  const user_id = userId;
+  let str = '';
+  for (let j = 1; j < randomNumList; j +=1 ) {
+    const list_id = j;
+    const list_name = faker.lorem.words();
+
+    for (let k = 1; k < randomNumRoom; k += 1 ) {
+      const room_id = Math.floor(Math.random()* 10000000) + 1;
+      str += `${user_id},${list_id},${list_name},${room_id}\n`;
+    }
+  }
+  return str;
+};
+
+const startWriting = (writeStream, encoding, dataFunction, done) => {
+  let i = start;
+  function writing() {
+    let canWrite = true;
+    do {
+      let data = dataFunction(i);
+      i +=1;
+      if (i === finish) {
+        writeStream.write(data, encoding, done);
+      } else {
+        canWrite = writeStream.write(data, encoding);
+      }
+    } while (i< finish && canWrite);
+    if (i < finish && canWrite) {
+      writeStream.once('drain', writing);
+    }
+  }
+  writing();
+};
+
+stream1.write(`room_id, photo_order, title, address, image_url, image_description\n`, 'utf-8');
+
+startWriting(stream1, 'utf-8', photosByRoom, () => {
+  stream1.end();
+});
+
+stream2.write(`user_id,list_id,list_name,room_id\n`, 'utf-8');
+
+startWriting(stream2, 'utf-8', listByUser, () => {
+  stream2.end();
+});
--- database/postSeed.js
+++ database/postSeed.js
@@ -4,10 +4,12 @@ const { argv } = require('yargs');
 
 const start = argv.start || 1; // change this later;
 const finish = argv.finish || 2;
+
 const filename1 = argv.output1 || 'postgresUsers.csv';
 const filename2 = argv.output2 || 'postgresRooms.csv';
 const filename3 = argv.output3 || 'postgresLists.csv';
 const filename4 = argv.output4 || 'postgresPhotos.csv';
+
 const stream1 = fs.createWriteStream(filename1);
 const stream2 = fs.createWriteStream(filename2);
 const stream3 = fs.createWriteStream(filename3);
@@ -29,14 +31,14 @@ const createRooms = () => {
 };
 
 const createList = (userId) => {
-  const randNumberRooms = Math.floor(Math.random() * 10);
-  const randNumberListName = Math.floor(Math.random() * 8);
+  const randNumberRooms = Math.floor(Math.random() * 8);
+  const randNumberListName = Math.floor(Math.random() * 5);
   const user_id = userId;
   let str = '';
   for (let i = 0; i < randNumberListName; i += 1) {
     const name = faker.lorem.words();
     for (let j = 0; j < randNumberRooms; j += 1) {
-      const room_id = Math.floor(Math.random() * 10000001);
+      const room_id = Math.floor(Math.random() * 1000000) + 1;
       str += `${user_id},${name},${room_id}\n`;
     }
   }
@@ -63,9 +65,7 @@ const startWriting = (writeStream, encoding, dataFunction, done) => {
     let canWrite = true;
     do {
       let data = dataFunction(i);
-      if( i % 1000000) {
-        console.log('');
-      }
+
       i += 1;
       // check if i === 0 so we would write and call `done`
       if (i === finish) {
