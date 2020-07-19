e556da774a6c170e8f7368098cc287f6fd21a868
erichen886 <erichen886@gmail.com>
2020-07-19 01:59:28 -0700

    updated postgres schema

--- database/schema.sql
+++ database/schema.sql
@@ -23,9 +23,13 @@ name varchar(80),
 room_id INTEGER REFERENCES rooms(id)
 );
 
+CREATE index user_id_room_id_list_index ON list (user_id, room_id);
+
 CREATE TABLE IF NOT EXISTS room_photos (
 id SERIAL NOT NULL PRIMARY KEY,
 image_url varchar (50) NOT NULL,
 image_desc varchar (70),
 room_id INTEGER REFERENCES rooms(id)
-)
+);
+
+CREATE index room_photos_index ON room_photos (room_id);
